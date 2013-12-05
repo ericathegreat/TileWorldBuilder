2943dbcef0881248f9130a4fb8e23ed73d5f24c4
ericathegreat <EricaTheGreat@gmail.com>
2013-12-05 22:35:45 +1100

    Create a map from an array & scroll with arrows

---
+++ Assets/Scripts/CameraKeyboardInput.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var movementSpeed : float = 1.0;
+
+function Start () {
+
+}
+
+function Update () {
+	var cameraMove = Input.GetAxis ("Horizontal");
+	if (cameraMove == -1) {
+		this.transform.position.x -= movementSpeed;
+		this.transform.position.z += movementSpeed;
+	}
+	if (cameraMove == 1) {
+		this.transform.position.x += movementSpeed;
+		this.transform.position.z -= movementSpeed;
+	}
+	
+	cameraMove = Input.GetAxis ("Vertical");
+	if (cameraMove == -1) {
+		this.transform.position.y -= movementSpeed;
+	}
+	if (cameraMove == 1) {
+		this.transform.position.y += movementSpeed;
+	}
+}
---
+++ Assets/Scripts/GroundMapRenderer.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+public var groundMap : GroundMap;
+public var tileLookup : Transform[];
+public var tileOffsetX : float;
+public var tileOffsetZ : float;
+
+function Start () {
+	DrawMap();
+}
+
+function DrawMap() {
+	for(var i=0; i<groundMap.map.length; i++)
+	{
+		for(var j=0; j<groundMap.map[i].length; j++)
+		{
+			var newTilePrefab : Transform = tileLookup[groundMap.map[i][j]];
+			var newTile : Transform = Instantiate(newTilePrefab, Vector3(i * tileOffsetX, this.transform.position.y, j * tileOffsetZ),Quaternion.identity);
+		}
+	}
+}
+
+function Update () {
+
+}
